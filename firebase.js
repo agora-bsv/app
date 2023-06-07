b261fd1f2ee570bc4d0f43cc1b4b7a9b264e2a70
ChatBSV <ab@alanborger.com>
2023-06-07 06:58:12 +0200

    agora 0.0.1

--- firebase.js
+++ firebase.js
@@ -1,24 +1,24 @@
 // firebase.js
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import { createContext, useContext, useEffect, useState } from 'react';
-import dotenv from 'dotenv';
-
-
-
-dotenv.config();
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCrV1H9MtTDqFSkmjpHERv-z9QmAeQyR-Q",
+  authDomain: "agora-icu.firebaseapp.com",
+  databaseURL: "https://agora-icu-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "agora-icu",
+  storageBucket: "agora-icu.appspot.com",
+  messagingSenderId: "145792888876",
+  appId: "1:145792888876:web:57931d3819684722d36189",
+  measurementId: "G-B0E791EVNQ"
 };
+
 
 firebase.initializeApp(firebaseConfig);
 
