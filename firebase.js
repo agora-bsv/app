123393aea9de8f120b8307bdf06d65b46c968405
ChatBSV <ab@alanborger.com>
2023-06-07 03:40:15 +0200

    agora

--- firebase.js
+++ firebase.js
@@ -4,10 +4,20 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import { createContext, useContext, useEffect, useState } from 'react';
-import { firebaseConfig } from './firebaseConfig';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  });
 }
 
 export const auth = firebase.auth();
