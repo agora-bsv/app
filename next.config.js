3d67f94c7a06d75cc9bb7d424af7015ade40150c
ChatBSV <ab@alanborger.com>
2023-06-12 01:37:06 +0200

    nextconfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  target: 'experimental-serverless-trace', // Enables serverless functions support
-  distDir: 'out', // Output directory for Next.js build
+  distDir: '.next', // Output directory for Next.js build
   serverless: true, // Tells Next.js to generate serverless pages
   webpack: (config, { isServer }) => {
     if (!isServer) {
